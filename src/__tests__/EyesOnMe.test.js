9520abd3d469e19d99672dba3a6fea82393de7fc
lubnaa <lubnaamuhammed@gmail.com>
2025-07-26 13:04:50 +0300

    final commit

--- src/__tests__/EyesOnMe.test.js
+++ src/__tests__/EyesOnMe.test.js
@@ -1,31 +1,19 @@
-import "@testing-library/jest-dom";
-import { render, screen, fireEvent } from "@testing-library/react";
-import EyesOnMe from "../components/EyesOnMe";
+import React from 'react';
 
-beforeEach(() => {
-  render(<EyesOnMe />);
-});
+function EyesOnMe() {
+  function handleFocus() {
+    console.log('Good!');
+  }
 
-test('displays a button with the text "Eyes on me"', () => {
-  expect(screen.queryByText(/Eyes on me/)).toBeInTheDocument();
-});
+  function handleBlur() {
+    console.log('Hey! Eyes on me!');
+  }
 
-test("focusing the button triggers console output", () => {
-  console.log = jest.fn();
+  return (
+    <div>
+      <button onFocus={handleFocus} onBlur={handleBlur}>Eyes on me</button>
+    </div>
+  );
+}
 
-  const button = screen.queryByText(/Eyes on me/);
-  fireEvent.focus(button);
-
-  expect(console.log).toHaveBeenCalled();
-  expect(console.log.mock.calls[0][0]).toBe("Good!");
-});
-
-test("removing focus (blur) on the button triggers console output", () => {
-  console.log = jest.fn();
-
-  const button = screen.queryByText(/Eyes on me/);
-  fireEvent.blur(button);
-
-  expect(console.log).toHaveBeenCalled();
-  expect(console.log.mock.calls[0][0]).toBe("Hey! Eyes on me!");
-});
+export default EyesOnMe;
--- src/__tests__/Keypad.test.js
+++ src/__tests__/Keypad.test.js
@@ -1,30 +1,15 @@
-import "@testing-library/jest-dom";
-import { render, fireEvent } from "@testing-library/react";
-import Keypad from "../components/Keypad";
+import React from 'react';
 
-let container;
+function Keypad() {
+  function handleChange() {
+    console.log('Entering password...');
+  }
 
-beforeEach(() => {
-  container = render(<Keypad />).container;
-});
+  return (
+    <div>
+      <input type="password" onChange={handleChange} />
+    </div>
+  );
+}
 
-test("displays one input", () => {
-  const input = container.querySelector("input");
-  expect(input).toBeInTheDocument();
-  expect(input.tagName).toBe("INPUT");
-});
-
-test("displays an input with the right input type", () => {
-  const input = container.querySelector("input");
-  expect(input.type).toBe("password");
-});
-
-test("typing in the input triggers console output", () => {
-  console.log = jest.fn();
-
-  const input = container.querySelector("input");
-  fireEvent.change(input, { target: { value: "123" } });
-
-  expect(console.log).toHaveBeenCalled();
-  expect(console.log.mock.calls[0][0]).toBe("Entering password...");
-});
+export default Keypad;
